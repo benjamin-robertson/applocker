d823222473c6a803bf8b914ab787e5c866d5585d
ben <benrobertson9876@gmail.com>
2022-12-28 17:23:13 +1100

    .

--- lib/facter/applocker_rules.rb
+++ lib/facter/applocker_rules.rb
@@ -5,6 +5,6 @@ Facter.add(:applocker_rules) do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-ApplockerPolicy -Effective -xml'
-    value = Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}))
+    Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}))
   end
 end
