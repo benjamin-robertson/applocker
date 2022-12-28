7273cb51abb8cd3a18a3451664c7ec8c8c6146f6
ben <benrobertson9876@gmail.com>
2022-12-28 18:07:19 +1100

    specific function

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,5 +34,5 @@ class applocker (
       'packaged_app_rules'     => $packaged_app_rules,}),
   }
   notify{"exec_applocker_rules lenght is ${exec_applocker_rules.length}":}
-  notify{"Applocker rules are ${xml_tohash($facts['applocker_rules'])}":}
+  notify{"Applocker rules are ${applocker::xml_tohash($facts['applocker_rules'])}":}
 }
