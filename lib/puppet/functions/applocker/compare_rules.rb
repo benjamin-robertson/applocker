1ce9668ce769471f00493649fba126b23fec49ad
ben <benrobertson9876@gmail.com>
2023-06-15 19:10:20 +1000

    check return

--- lib/puppet/functions/applocker/compare_rules.rb
+++ lib/puppet/functions/applocker/compare_rules.rb
@@ -5,7 +5,7 @@ Puppet::Functions.create_function(:"applocker::compare_rules") do
   dispatch :compare_rules do
     param 'Hash', :rules
     param 'Hash', :desired_rules
-    return_type 'Array'
+    return_type 'Hash'
   end
   # the function below is called by puppet and and must match
   # the name of the puppet function above. You can set your
@@ -15,6 +15,19 @@ Puppet::Functions.create_function(:"applocker::compare_rules") do
   def compare_rules(rules, desired_rules)
     rule_collection = rules['RuleCollection']
     desired_collection = desired_rules['RuleCollection']
+
+    # Get Appx rules
+    appx_a = get_rule_section('Appx', rule_collection)
+    appx_b = get_rule_section('Appx', desired_collection)
+    appx_a
+  end
+
+  def get_rule_section(type, rules)
+    rules.each do | element |
+      if element['Type'] == type
+        return element
+      end
+    end
   end
 
   # you can define other helper methods in this code block as well
