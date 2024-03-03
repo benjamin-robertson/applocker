c933d817d5fbd5aabbf6bc42a3587c754cf0307c
ben <benrobertson9876@gmail.com>
2024-03-04 10:04:15 +1100

    added notify

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,12 +39,14 @@ class applocker (
 
   # Break down structure using function, We want to retrieve all the names of each rules type and return
   $rule_results = applocker::extract_rules($hash_policy)
+  notify { "${rule_results}": }
 
   # Check if we have any rules set
   $rule_results_final = defined('$rule_results[\'name_to_id\']') ? {
     true    => $rule_results['name_to_id'],
     default => {},
   }
+  notify { "${rule_results_final}": }
 
   # Generate id for each rule, or get from existing rule. 
   $exec_applocker_rules_with_id = applocker::get_id($exec_applocker_rules, $rule_results_final)
