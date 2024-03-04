0958b3ae39356be92296c9a76b9f6dd9a4441653
ben <benrobertson9876@gmail.com>
2024-03-04 11:51:53 +1100

    return more info

--- lib/puppet/functions/applocker/compare_rules.rb
+++ lib/puppet/functions/applocker/compare_rules.rb
@@ -71,7 +71,7 @@ Puppet::Functions.create_function(:"applocker::compare_rules") do
                'failing_rule' => 'Script' }
     end
 
-    return { 'Result' => true }
+    return { 'Result' => true, 'exe_a' => exe_a, 'exe_b' => exe_b, 'exe_result' => exe_result }
 
   end
 
