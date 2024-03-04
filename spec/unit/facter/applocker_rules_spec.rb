a4774cec9a55323b06bba195e9eff70677547176
ben <benrobertson9876@gmail.com>
2024-03-04 18:42:23 +1100

    work so far

--- spec/unit/facter/applocker_rules_spec.rb
+++ spec/unit/facter/applocker_rules_spec.rb
@@ -10,9 +10,13 @@ describe :applocker_rules, type: :fact do
   before :each do
     # perform any action that should be run before every test
     Facter.clear
+    # allow(Facter::Util::Resolution).to receive(:exec).with('C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -command Get-ApplockerPolicy -Effective -xml').and_return('<AppLockerPolicy Version="1" />')
+    allow(Facter::Util::Resolution).to receive(:exec).and_return('<AppLockerPolicy Version="1" />')
   end
 
   it 'returns a value' do
-    expect(fact.value).to eq(nil)
+    allow(Facter::Util::Resolution).to receive(:exec).and_return('<AppLockerPolicy Version="1" />')
+    expect(fact.value).to eq('<AppLockerPolicy Version="1" />')
+    # expect(Facter.fact(:applocker_rules).value).to eq('<AppLockerPolicy Version="1" />')
   end
 end
