03f3e4cef7750c56f79e7bc969172dda2961b6d9
ben <benrobertson9876@gmail.com>
2024-03-05 12:26:52 +1100

    push code with updates

--- lib/facter/applocker_rules.rb
+++ lib/facter/applocker_rules.rb
@@ -7,9 +7,6 @@ Facter.add(:applocker_rules) do
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-ApplockerPolicy -Effective -xml'
-    # Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}))
     Facter::Core::Execution.execute(%(#{powershell} -command "#{command}"))
-    # output, status = Open3.capture2(%(#{powershell} -command "#{command}"))
-    # output
   end
 end
