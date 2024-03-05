7dfe629d904c34c93f4b22cfdc2ae984baf8170c
ben <benrobertson9876@gmail.com>
2024-03-05 12:22:44 +1100

    Test new fact

--- lib/facter/applocker_rules.rb
+++ lib/facter/applocker_rules.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+require 'open3'
+
 Facter.add(:applocker_rules) do
   confine kernel: 'windows'
   setcode do
     powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
     command = 'Get-ApplockerPolicy -Effective -xml'
-    Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}))
+    # Facter::Util::Resolution.exec(%(#{powershell} -command "#{command}))
+    Facter::Core::Execution.execute(%(#{powershell} -command "#{command}"))
+    # output, status = Open3.capture2(%(#{powershell} -command "#{command}"))
+    # output
   end
 end
--- spec/unit/facter/applocker_rules_spec.rb
+++ spec/unit/facter/applocker_rules_spec.rb
@@ -11,12 +11,17 @@ describe :applocker_rules, type: :fact do
     # perform any action that should be run before every test
     Facter.clear
     # allow(Facter::Util::Resolution).to receive(:exec).with('C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe -command Get-ApplockerPolicy -Effective -xml').and_return('<AppLockerPolicy Version="1" />')
-    allow(Facter::Util::Resolution).to receive(:exec).and_return('<AppLockerPolicy Version="1" />')
+    powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+    command = 'Get-ApplockerPolicy -Effective -xml'
+
+    allow(Facter::Core::Execution).to receive(:execute).and_return('<AppLockerPolicy Version="1" />')
+    # allow(Open3).to receive(:capture2).with(%(#{powershell} -command "#{command}")).and_return('<AppLockerPolicy Version="1" />')
+    # allow(Open3).to receive(:capture2).and_return('<AppLockerPolicy Version="1" />') 
   end
 
   it 'returns a value' do
-    allow(Facter::Util::Resolution).to receive(:exec).and_return('<AppLockerPolicy Version="1" />')
+    # allow(Open3).to receive(:capture2).and_return('<AppLockerPolicy Version="1" />')
     expect(fact.value).to eq('<AppLockerPolicy Version="1" />')
-    # expect(Facter.fact(:applocker_rules).value).to eq('<AppLockerPolicy Version="1" />')
+    # expect(fact.value).to eq(nil)
   end
 end
